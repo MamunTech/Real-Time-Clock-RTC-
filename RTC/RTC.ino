0b92a2c2a0047632d9f7586320ca80561243d47e
MamunTech <amamun860@gmail.com>
2018-06-26 21:43:13 +0600

    First commit

---
+++ RTC/RTC.ino
@@ -0,0 +1,33 @@
+//link: https://www.elecrow.com/wiki/index.php?title=Tiny_RTC
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
+
+void setup () {
+    Serial.begin(9600);
+    Wire.begin();
+    RTC.begin();
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+}
+void loop () {
+    DateTime now = RTC.now(); 
+    Serial.print("Date: ");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print('     ');
+    Serial.print("Time: ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println(); 
+    delay(1000);
+}
